6bab951c6e56d5e5547e5fd3a417a1c5ac5164e9
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2018-02-23 19:07:08 -0600

    first commit

---
+++ add.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<h1>A small example page to insert some data in to the MySQL database using PHP</h1>
+
+<form action="insert.php" method="post">
+Firstname: <input type="text" name="first_name" /><br><br>
+Lastname: <input type="text" name="last_name" /><br><br> 
+Email: <input type="text" name="email" /><br><br>
+Message: <input type="text" name="message" /><br><br>
+<input type="submit" />
+</form>
+
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,24 @@
+<?php
+$servern
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("ame = "localhost";
+$username = "";
+$password = "";
+$dbname = "";
+
+// Create connectionConnection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email, message)
+VALUES ('$_POST[first_name]', '$_POST[last_name]', '$_POST[email]', '$_POST[message]')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
