bf4ff334f965bbc3beda027c7320054ece7cc78d
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2018-03-06 19:44:02 -0600

    test

--- book.php
+++ book.php
@@ -18,17 +18,19 @@ if ($conn->connect_error) {
 
 $sql = "select * from books where book_id='$_GET[book_id]'";
 
+$echo sql
+
 $result = $conn->query($sql);
 
 
-if ($result->num_rows > 0) {
+//if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
+//    while($row = $result->fetch_assoc()) {
+//        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+//    }
+//} else {
+//    echo "0 results";
+//}
 
 $conn->close();
 ?>
