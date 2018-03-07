ae4c51893dcbd537970621bf3e722a6f92658932
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2018-03-06 20:11:33 -0600

    added mysql query in book page

--- book.php
+++ book.php
@@ -6,9 +6,13 @@
 $servername = "localhost";
 $username = "srm3536";
 $password = "inf385m";
-$dbname = "srm3536";
+$dbname = "lofilib";
 
-// Create connectionConnection failed: " . $conn->connect_error);
+$sql = "select * from books where book_id='$_GET[book_id]'";
+
+echo $sql;
+
+// Create connection
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -16,21 +20,25 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "select * from books where book_id='$_GET[book_id]'";
-
-$echo sql
 
 $result = $conn->query($sql);
 
+//echo $result->num_rows;
 
-//if ($result->num_rows > 0) {
-    // output data of each row
-//    while($row = $result->fetch_assoc()) {
-//        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-//    }
-//} else {
-//    echo "0 results";
-//}
+
+echo "<br / ><br />";
+
+
+if ($result->num_rows > 0) {
+//     output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo '<br /> Title: ' . $row['title'];
+        echo '<br /> Authors: ' . $row['authors'];
+        echo '<br /> Filename: ' . $row['filename'];
+    }
+} else {
+    echo "0 results";
+}
 
 $conn->close();
 ?>
@@ -39,4 +47,4 @@ $conn->close();
 
 
 </body>
-</html>
+           
